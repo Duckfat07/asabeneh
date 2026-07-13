5dc0f1c41211bc9fec90b0881c5a5458d819cfe6
Ethan Kim <kim.ethanj@gmail.com>
2026-07-12 22:08:29 -0400

    Start Day 1 Python

---
+++ day01/main.py
@@ -0,0 +1,12 @@
+#day 1 of python learning! let's fucking go!
+print("Hello World!") # print Hello World in python
+print("Lionel Messi is the GOAT of soccer")
+
+print(5+7) # addition
+print(30-20)
+print(60*5)
+print(20/4)
+print(25/4) # will return a float, a decimal
+print(2**4) # exponents 
+print(19%3) # modulus, returns the remainder of a division operation
+print(25//3) # floor division, divides two numbers and rounds the result down to the nearest whole number
