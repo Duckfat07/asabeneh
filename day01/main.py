2c8377aa4724678522e9762ec73c9857adab5324
Ethan Kim <kim.ethanj@gmail.com>
2026-07-14 17:15:04 -0400

    finished prints and data types

--- day01/main.py
+++ day01/main.py
@@ -10,3 +10,14 @@ print(25/4) # will return a float, a decimal
 print(2**4) # exponents 
 print(19%3) # modulus, returns the remainder of a division operation
 print(25//3) # floor division, divides two numbers and rounds the result down to the nearest whole number
+
+# checking data types
+print(type(100)) # int
+print(type(2-3j)) # complex, j has a special meaning in python. It represents the imaginary unit square root of -1
+print(type(30.99)) # float, decimal
+print(type("The World Cup is the best soccer tournament on the planet!")) # string, quotations
+print(type(False)) # boolean
+print(type(True)) # boolean
+print(type([10,11,12,13,14])) # list, uses square brackets!
+print(type({'color':'blue', 'size':'large'})) # dictionary, curly brackets means dictionary!! 
+print(type((1,2,3,4)))
