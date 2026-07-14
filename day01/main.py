07ed4c070e3dba0a31376e7058b69391872b137d
Ethan Kim <kim.ethanj@gmail.com>
2026-07-14 17:44:32 -0400

    data types done

--- day01/main.py
+++ day01/main.py
@@ -13,11 +13,11 @@ print(25//3) # floor division, divides two numbers and rounds the result down to
 
 # checking data types
 print(type(100)) # int
-print(type(2-3j)) # complex, j has a special meaning in python. It represents the imaginary unit square root of -1
+print(type(2-3j)) # complex, j has a special meaning in python. It represents the imaginary unit square root of -1, j must always have a coefficient before it
 print(type(30.99)) # float, decimal
 print(type("The World Cup is the best soccer tournament on the planet!")) # string, quotations
 print(type(False)) # boolean
 print(type(True)) # boolean
 print(type([10,11,12,13,14])) # list, uses square brackets!
 print(type({'color':'blue', 'size':'large'})) # dictionary, curly brackets means dictionary!! 
-print(type((1,2,3,4)))
+print(type((1,2,3,4))) # tuple, uses parentheses
