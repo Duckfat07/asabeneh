510faf6081c72e93b97cf57563386929a0000149
Ethan Kim <kim.ethanj@gmail.com>
2026-07-27 21:32:38 -0400

    variable declaration and printing variables

---
+++ day03/main.py
@@ -0,0 +1,35 @@
+# variables in python
+# assigning a certain data type to a variable is called variable declaration
+first_name = 'Ethan'
+last_name = 'Kim'
+country = 'The United States of America'
+age = 18
+is_male = True
+features = ['athletic','intelligent','hard worker'] # this is an example of a list, uses square brackets 
+personal_information = {
+    'name':'Ethan Kim',
+    'country_of_origin':'Korea',
+    'city':'New York City'
+}
+
+# Printing the values stored in variables
+print('First name:', first_name)
+print('Last name:', last_name)
+print('length of first name:', len(first_name))
+
+name_of_user = input('What is your name?')
+age_of_user = input('How old are you?')
+
+print(name_of_user, age_of_user)
+
+year = 2026
+print(type(year))
+
+years_till_2028 = 2028 - year
+print(years_till_2028)
+
+
+# declaring multiple variables on one line
+animal, number_of_legs, is_farm = 'pig', 4, True
+print(animal, number_of_legs, is_farm)
+
