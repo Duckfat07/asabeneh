0e76d219664a9f4e41fca914560a0ecfb3efd11c
Ethan Kim <kim.ethanj@gmail.com>
2026-08-10 17:46:10 -0400

    finished escape sequences in strings

--- day04/main.py
+++ day04/main.py
@@ -29,3 +29,49 @@ print(len(first_name))
 print(len(first_name) > len(last_name)) # true
 
 # escape sequences in strings
+# \n: new line
+# \t: tab means (8 spaces)
+# \\: Back slash
+# \': Single quote (')
+# \": Double quote (")
+
+print('Everyone is enjoying learning python.\nAre you') # line break
+print('Python Files\tDays\tDirectories') # adding tab space or 4 spaces
+print('Every programming language starts with \"Hello World\"') # to write double quotations within a string
+print('This is a backslash symbol (\\)') # to write a backslash
+print('Days\tTopics\tExercises')
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35') # the ouput of this should be a grid, with Days, Topics, and Exercises as headlines
+
+# alternate method of string formatting (% operator)
+# %s - String (or any object with a string representation)
+# %d - Integers 
+# %f - Floating point numbers
+# "%.number of digitsf" - Floating point numbers with fixed precision
+
+# strings only
+first_name = 'Ethan'
+last_name = 'Kim'
+language = 'Python'
+formatted_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+print(formatted_string)
+
+# strings and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formatted_string = 'The area of circle with a radius %d is %.2f.' %(radius, area)
+
+python_libraries = ['Django', 'Flask', 'NumPy', 'Madplotlib', 'Pandas']
+formatted_string = 'The following are python libraries:%s' % (python_libraries) # the following are python libraries:['Django', 'Flask', 'NumPy','Matplotlib','Pandas']
+
+# new style string formatting (str.format)
+first_name = 'Ethan'
+last_name = 'Kim'
+language = 'Python'
+formatted_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
+print(formatted_string)
+
+
