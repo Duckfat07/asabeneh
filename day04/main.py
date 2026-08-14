ab01fd4b580c0542bb7588595986243f96e45b43
Ethan Kim <kim.ethanj@gmail.com>
2026-08-13 20:55:24 -0400

    old and new style string formats

--- day04/main.py
+++ day04/main.py
@@ -74,4 +74,10 @@ language = 'Python'
 formatted_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
 print(formatted_string)
 
+a = 4
+b = 3
+print('{} + {} = {}'.format(a, b, a+b))
+print('{} - {} = {}'.format(a, b, a-b))
+print('{} * {} = {}'.format(a, b, a*b))
+
 
