e8d87429bfcba632fe4a42d5de08e1240f71e6c7
Ethan Kim <kim.ethanj@gmail.com>
2026-09-22 23:01:28 -0400

    day05 lists started. LFG

---
+++ day05/main.py
@@ -0,0 +1,19 @@
+# lists
+lst = list()
+
+empty_list = list() # this list contains absolutely nothing
+print(len(empty_list)) #0 
+
+# two ways to create lists. 
+# first one is by using the built-in list() constructor
+# second one is by using square brackets []
+lst = []
+
+fruits = ['grapes', 'oranges', 'strawberries', 'mangoes']
+print(len(fruits))
+print(fruits)
+
+countries = ['Finland', 'Sweden', 'United States']
+print(len(countries))
+print(countries)
+
