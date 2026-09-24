6747dce3ee3fd5aced4ec3e742bce21e04f50474
Ethan Kim <kim.ethanj@gmail.com>
2026-09-23 23:09:29 -0400

    day05 continued.

--- day05/main.py
+++ day05/main.py
@@ -1,3 +1,9 @@
+# four collection data types in python
+#list: ordered and mutable (able to be changed), allows duplicates. uses square brackets []
+# tuple: ordered and immutable, allows duplicates. uses parentheses and commas
+# set: unordered, unindexed, immutable, but can add new items to the set. duplicates not allowed. curly brackets and commas. 
+# dictionary: unordered, mutable, and indexed. No duplicate members. curly brackets and colons
+
 # lists
 lst = list()
 
@@ -16,4 +22,12 @@ print(fruits)
 countries = ['Finland', 'Sweden', 'United States']
 print(len(countries))
 print(countries)
+
+lst = ['Ethan', 18, {'country': 'America', 'city':'New York'}] # lists can contain other data types. 
+# lists are square brackets, dictionaries and sets utilize curly brackets
+
+# access items using postive indexing
+teams = ['Tottenham', 'Man City', 'Everton', 'Liverpool']
+print(teams[0])
+print(teams[1] + ' ' + 'and' + ' ' + teams[2])
 
